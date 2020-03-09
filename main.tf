5aa37d36a2a4ec9f4932fd94d5ddbd3d1168b501
Tudor Toma <tudortoma@tencent.com>
2020-03-09 18:11:19 +0100

    added sg + rules

--- main.tf
+++
@@ -1,6 +0,0 @@
-# Configure the TencentCloud Provider
-provider "tencentcloud" {
-  secret_id  = "AKID4wb1C9iyWpvlMeheLn24rQqIagYDmsqX"
-  secret_key = "e6JH1YqslRvAAvSloiPwFKAm9FBwVpti"
-  region     = var.region
-}
